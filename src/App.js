82452a78f3df47feb9aaaa2e56f53f11075a2997
thomasrosen <thomas.rosen@me.com>
2024-08-17 17:43:26 +0200

    clean up to start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <h1>Yearline</h1>
+        <p>A super simple way to see your whole year at a glance. Daily dots show what’s going on, how you’re feeling or how the year is going to be.</p>
       </header>
-    </div>
+    </main>
   );
 }
 
